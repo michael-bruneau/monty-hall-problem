75375abb1f57a315cfa12412d048d19238fbb02f
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2024-05-02 19:30:09 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+/* Copyright (c) 2020 MTHS All rights reserved
+ *
+ * Created by: Michael Bruneau
+ * Created on: May 2024
+ * This program runs simulator of monty hall problem
+*/
+
+// variables
+let randomNumber: number = 0
+
+while (true) {
+  if (input.buttonIsPressed(Button.A) == true) {
+    randint(1, 3)
+    basic.showNumber(randomNumber)          
+  }
+}
