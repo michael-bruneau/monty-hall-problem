606b62765736f1ec18676c6751c24bfe7c057a3c
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2024-05-03 12:02:32 -0400

    control group

--- main.ts
+++ main.ts
@@ -14,6 +14,7 @@ let wrongDoorRevealed: number = null
 let doorSwitched: number = null
 let winCounter: number = null
 let loopCounter: number = 0
+let checkingWhichDoorNewDoorWasPicked: number = null
 
 while (true) {
   if (input.buttonIsPressed(Button.A) == true) {
@@ -62,6 +63,65 @@ while (true) {
   }
 
   if (input.buttonIsPressed(Button.B) == true) {
-    basic.showNumber(winCounter)
+    while (loopCounter <= 1000) {
+      // picks random door
+      winningDoor = randint(1, 3)
+
+      // find out were loosingDoor1 is
+      if (winningDoor - 1 > 0) {
+        lossingDoor1 = 1
+      } else {
+        lossingDoor1 = 2
+      }
+
+      // find out were lossingDoor2 is
+      lossingDoor2 = 6 - lossingDoor1 - winningDoor
+
+      // doorPicked
+      doorPicked = randint(1, 3)
+
+      // the wrong door that is revealed
+      if (doorPicked - 1 > 0) {
+        if (winningDoor - 1 > 0) {
+          wrongDoorRevealed = 1
+        }
+      }
+      if (doorPicked - 2 > 0 || doorPicked - 2 < 0) {
+        if (winningDoor - 2 > 0 || winningDoor - 2 < 0) {
+          wrongDoorRevealed = 2
+        }
+      }
+      if (doorPicked - 3 > 0 || doorPicked - 3 < 0) {
+        if (winningDoor - 3 > 0 || winningDoor - 3 < 0) {
+          wrongDoorRevealed = 3
+        }
+      }
+
+      // doorSwitched
+      if (wrongDoorRevealed == 1) {
+        doorSwitched = randint(2, 3)
+      }
+
+      if (wrongDoorRevealed == 2) {
+        checkingWhichDoorNewDoorWasPicked = randint(1,2) 
+        if (checkingWhichDoorNewDoorWasPicked == 1) {
+          doorSwitched = 1
+        }
+        if (checkingWhichDoorNewDoorWasPicked == 2) {
+          doorSwitched = 3
+        }
+      }
+
+      if (wrongDoorRevealed == 3) {
+        doorSwitched = randint(1,2)
+      }
+
+      if (doorSwitched == winningDoor) {
+        winCounter = winCounter + 1
+      }
+      loopCounter = loopCounter + 1
+    }
   }
 }
+
+
